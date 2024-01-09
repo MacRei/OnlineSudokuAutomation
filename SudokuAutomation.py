3ddf8926c9f02b4bb2266fab0800a57ae43e5e2c
MacRei <126007422+MacRei@users.noreply.github.com>
2024-01-09 09:43:47 -0500

    Update SudokuAutomation.py

--- SudokuAutomation.py
+++ SudokuAutomation.py
@@ -1,6 +1,5 @@
 print("Importing modules...")
-import SudokuWebScraper
-import SudokuSolver
+import SudokuWebScraper, SudokuSolver
 import numpy as np
 from time import sleep
 import pyautogui as robot
@@ -9,29 +8,26 @@ print("Retrieving puzzle...")
 puzzle, puzzleID = SudokuWebScraper.GetPuzzle()
 puzzle = np.array(puzzle)
 print(f"Puzzle from \"https://www.menneske.no/sudoku/eng/\"\nPuzzle ID is {puzzleID}.\nPuzzle:")
-print(puzzle)
+print(puzzle, "\nSolving puzzle...")
 puzzle = SudokuSolver.SolvePuzzle(puzzle)
-if "_" in puzzle:
-    print("Can't Solve")
-else:
-    print("Solved")
-    robot.moveTo(1050, 1030)
-    robot.click()
-    sleep(2)
-    robot.typewrite(f"https://www.menneske.no/sudoku/eng/solve.html?number={puzzleID}")
-    robot.press("enter")
-    sleep(2)
-    robot.moveTo(1900, 200)
-    robot.dragTo(1900, 350, 1, button="left") #640 240
+print("Solved")
+
+robot.moveTo(1050, 1030) #This code will not work as it should on other computers because of dimensions of screen and location of apps on taskbar.
+robot.click()
+sleep(2)
+robot.typewrite(f"https://www.menneske.no/sudoku/eng/solve.html?number={puzzleID}")
+robot.press("enter")
+sleep(4) #This 4 second sleep function is used to allow the webpage time to load.
+robot.moveTo(1900, 200)
+robot.dragTo(1900, 350, 1, button="left")
+rowX = 0
+columnY = 0
+for row in puzzle:
     rowX = 0
-    columnY = 0
-    for row in puzzle:
-        rowX = 0
-        for square in row:
-            robot.moveTo(640 + (rowX * 65), 240 + (columnY * 65))
-            robot.click()
-            robot.typewrite(square)
-            rowX += 1
-            #sleep(1)
-        columnY += 1
-    robot.press("enter")
+    for square in row:
+        robot.moveTo(660 + (rowX * 65), 260 + (columnY * 65))
+        robot.click()
+        robot.typewrite(square)
+        rowX += 1
+    columnY += 1
+robot.press("enter")
